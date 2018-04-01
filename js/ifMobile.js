de9149d34ca41f9e896994187b5262799dd9d847
Zoro <swftapocalypse1@gmail.com>
2018-04-01 09:43:54 -0400

    New checks
    Added a mobile check and also made the discord redirect not have .html at the end of it.

---
+++ js/ifMobile.js
@@ -0,0 +1,2 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    alert('This site is not meant to be run on mobile devices!') }
