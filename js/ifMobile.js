9718268a3230f3cb3aa21df1b524a8c0773f2953
Zoro <swftapocalypse1@gmail.com>
2018-04-02 13:26:16 -0400

    Checks
    adding a redirect if the device it will redirect to our youtube channel since the website is not meant to be ran on mobile devices. mobile support will be made in the future but for now nothing is really meant to be ran on mobile.

--- js/ifMobile.js
+++ js/ifMobile.js
@@ -1,2 +1,5 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    alert('This site is not meant to be run on mobile devices!') }
+    alert('This site is not meant to be run on mobile devices!') 
+window.location.href = "https://www.youtube.com/channel/UCkw6lUQIfjMSZ-GVwKR8O3Q?view_as=subscriber";
+
+}
