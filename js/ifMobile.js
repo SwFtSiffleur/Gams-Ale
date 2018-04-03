ca16815de92e94de863528e98c525c51384acbbf
Zoro <swftapocalypse1@gmail.com>
2018-04-02 20:12:01 -0400

    Update
    Added 
      -Added new font on the buttons,
      -Fixed some sintax issues that were bugging me

--- js/ifMobile.js
+++ js/ifMobile.js
@@ -1,5 +1,4 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    alert('This site is not meant to be run on mobile devices!') 
-window.location.href = "https://www.youtube.com/channel/UCkw6lUQIfjMSZ-GVwKR8O3Q?view_as=subscriber";
-
+    alert('This site is not meant to be run on mobile devices!\nRedirecting....');
+    window.location.href = "https://www.youtube.com/channel/UCkw6lUQIfjMSZ-GVwKR8O3Q?view_as=subscriber";
 }
